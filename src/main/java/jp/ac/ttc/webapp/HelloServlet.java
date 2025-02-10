6832a70f730b783e3af09d53746df658e160862a
masaya <mt23304012@ga.ttc.ac.jp>
2025-02-10 14:24:50 +0900

    no message

--- src/main/java/jp/ac/ttc/webapp/HelloServlet.java
+++ src/main/java/jp/ac/ttc/webapp/HelloServlet.java
@@ -3,10 +3,10 @@ package jp.ac.ttc.webapp;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
